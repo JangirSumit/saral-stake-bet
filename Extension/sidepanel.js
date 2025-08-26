3a97ec79e7b82e13184ad106310ed5a303c8c867
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2025-08-26 22:23:27 +0530

    Update side panel HTML and refactor sidepanel.js to improve betting configuration and comment out save functionality

--- Extension/sidepanel.js
+++ Extension/sidepanel.js
@@ -7,17 +7,38 @@ document.addEventListener("DOMContentLoaded", () => {
   const crashTimes = document.getElementById("crashTimes");
   const saveBtn = document.getElementById("saveBtn");
 
-  saveBtn.addEventListener("click", () => {
-    const betData = {
-      amount: betAmount.value,
-      cashout: cashoutAt.value,
-      loss: onLoss.value,
-      win: onWin.value,
-      stopCrashAt: crashAt.value,
-      stopCrashTimes: crashTimes.value
-    };
-    chrome.storage.local.set({ betData }, () => {
-      console.log('Bet started with stop rules');
-    });
-  });
+  const gameSidebar = document.querySelector(
+    "[data-testid='game-frame'] .game-sidebar"
+  );
+  const StartBetButton = document.querySelector(
+    "[data-testid='game-frame'] .game-sidebar > button"
+  );
+  const statusSpan = StartBetButton.querySelector("span");
+  const betAmountInput = document.querySelector(
+    "[data-testid='game-frame'] [data-testid='input-game-amount']"
+  );
+  const profitInput = document.querySelector(
+    "[data-testid='game-frame'] [data-testid='profit-input']"
+  );
+  const cashoutInput = [
+    ...gameSidebar.querySelectorAll("label span[slot='label']"),
+  ]
+    .find((el) => el.textContent.trim() === "Cashout At")
+    ?.closest("label")
+    ?.querySelector("input");
+
+
+  // saveBtn.addEventListener("click", () => {
+  //   const betData = {
+  //     amount: betAmount.value,
+  //     cashout: cashoutInput.value,
+  //     loss: onLoss.value,
+  //     win: onWin.value,
+  //     stopCrashAt: crashAt.value,
+  //     stopCrashTimes: crashTimes.value,
+  //   };
+  //   chrome.storage.local.set({ betData }, () => {
+  //     console.log("Bet started with stop rules");
+  //   });
+  // });
 });
